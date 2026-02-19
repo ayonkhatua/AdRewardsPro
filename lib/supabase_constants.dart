3c78d43772966d49a0cd1b0062942fa249d291ac
ayonkhatua <ayonkhatua3011@gmail.com>
2026-02-19 19:05:18 +0530

    Fixed keystore password completely

--- lib/supabase_constants.dart
+++ lib/supabase_constants.dart
@@ -1,5 +1,6 @@
 class SupabaseConfig {
   // TODO: Yahan apni asli details replace karein
+  
   // Ye aapko Supabase Dashboard -> Settings -> API me milengi.
   // ==========================================
 
